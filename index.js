e2c738e054ecbab47fabd4f14ee53cfb639829ed
Sealcakes <taylorcseal@gmail.com>
2023-02-06 17:19:10 -0600

    adds inquirer questions for employees/managers/engineers/interns

--- index.js
+++ index.js
@@ -2,15 +2,93 @@ const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+const inquirer = require('inquirer');
 
 
-let employee = new Employee('Steve', 0030, 'steve@notanemail.com');
-let manager = new Manager('Mark', 0010, 'mark@notanemail.com', 103);
-let engineer = new Engineer('Taylor', 0020, 'taylor@notanemail.com', 'github.com/sealcakes');
-let intern = new Intern('Sacha', 0040, 'sacha@notanemail.com', 'Southern Methodist University');
+let employeeQuestions = [
+    {
+        type: 'input',
+        message: 'What is the name of the employee?',
+        name: 'employeeName',
+    },
 
+    {
+        type: 'input',
+        message: "What is the employee's ID number?",
+        name: 'employeeId'
+    },
 
-console.log(`${employee.name}, ${employee.email}, ${employee.id}, ${employee.getRole()}, ${employee.getEmail()}`);
-console.log(`${manager.getName()}, ${manager.getRole()}, ${manager.officeNumber}`);
-console.log(`${engineer.getName()}, ${engineer.getRole()}, ${engineer.getGithub()}`);
-console.log(`${intern.getName()}, ${intern.getRole()}, ${intern.getSchool()}`);
+    {
+        type: 'input',
+        message: "What is the employee's email address?",
+        name: 'employeeEmail'
+    },
+
+    {
+        type: "list",
+        message: "Which type of employee are you creating?",
+        name: "employeeChoice",
+        choices: [
+            'Employee',
+            'Manager',
+            'Engineer',
+            'Intern'
+        ]
+    }
+]
+
+let managerQuestions = [
+    {
+        type: 'input',
+        message: "What is the Manager's office number?",
+        name: "managerOfficeNumber"
+    }
+]
+
+let engineerQuestions = [
+    {
+        type: 'input',
+        message: "What is the Engineer's GitHub username?",
+        name: 'githubUsername'
+    }
+]
+
+let internQuestions = [
+    {
+        type: 'input',
+        message: "What school is the Intern currently enrolled at?",
+        name: 'internSchool'
+    }
+]
+
+function init() {
+    inquirer.prompt(employeeQuestions)
+    .then(function(answers) {
+        if(answers.employeeChoice === 'Manager') {
+            
+            inquirer.prompt(managerQuestions)
+            .then(function(managerAnswers) {
+                console.log(answers)
+                console.log(managerAnswers)
+            })
+        } else if(answers.employeeChoice === 'Engineer') {
+
+            inquirer.prompt(engineerQuestions)
+            .then(function(engineerAnswers) {
+                console.log(answers)
+                console.log(engineerAnswers)
+            })
+        } else if(answers.employeeChoice === 'Intern') {
+
+            inquirer.prompt(internQuestions)
+            .then(function(internAnswers) {
+                console.log(answers)
+                console.log(internAnswers)
+            })
+        } else {
+            console.log(answers)
+        }
+    })
+}
+
+init();
